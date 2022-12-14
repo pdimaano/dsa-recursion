0bfc582df94545b9811c612d419fa9975c2d0c67
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-12-13 17:00:16 -0800

    Finish up to further study

--- recursion.js
+++ recursion.js
@@ -34,19 +34,52 @@ function find(arr, val) {
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
-function isPalindrome(str) {}
+function isPalindrome(str) {
+  if (str.length === 0) return true;
+
+  const thisVal = str[0];
+  const mirroredVal = str[str.length - 1];
+  return thisVal === mirroredVal && isPalindrome(str.slice(1, str.length - 1));
+}
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {}
+function revString(str) {
+  if (str.length === 0) return "";
+
+  return revString(str.slice(1)) + str[0];
+}
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {}
+// function findIndex(arr, val, idx = 0) {
+//   if(idx === arr.length) return -1;
+//   if(arr[idx] === val) return idx;
+
+//   return findIndex(arr, val, idx + 1);
+// }
+
+function findIndex(arr, val) {
+  if(arr.length === 0) return -1;
+  if(arr[0] === val) return 0;
+
+  const foundIndex = findIndex(arr.slice(1), val);
+
+  return foundIndex === -1 ? -1 : foundIndex + 1;
+}
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {}
+function gatherStrings(obj) {
+  let strArr = [];
+
+  for(let key in obj) {
+    if(typeof obj[key] === "string") strArr.push(obj[key]);
+    if(typeof obj[key] === "object") strArr.push(...gatherStrings(obj[key]));
+  }
+
+  return strArr;
+}
 
 // FURTHER STUDY
 
--- recursion.test.js
+++ recursion.test.js
@@ -49,78 +49,78 @@ describe("find", function() {
   });
 });
 
-// describe("isPalindrome", function() {
-//   it("returns true if the string is a palindrome", function() {
-//     expect(isPalindrome("")).toBe(true);
-//     expect(isPalindrome("x")).toBe(true);
-//     expect(isPalindrome("noon")).toBe(true);
-//     expect(isPalindrome("tacocat")).toBe(true);
+describe("isPalindrome", function() {
+  it("returns true if the string is a palindrome", function() {
+    expect(isPalindrome("")).toBe(true);
+    expect(isPalindrome("x")).toBe(true);
+    expect(isPalindrome("noon")).toBe(true);
+    expect(isPalindrome("tacocat")).toBe(true);
 
-//   });
+  });
 
-//   it("returns false if the string is not a palindrome", function() {
-//     expect(isPalindrome("az")).toBe(false);
-//     expect(isPalindrome("tacodog")).toBe(false);
-//   });
-// });
+  it("returns false if the string is not a palindrome", function() {
+    expect(isPalindrome("az")).toBe(false);
+    expect(isPalindrome("tacodog")).toBe(false);
+  });
+});
 
-// describe("findIndex", function() {
-//   let animals = ["duck", "cat", "cat", "pony"];
+describe("findIndex", function() {
+  let animals = ["duck", "cat", "cat", "pony"];
 
-//   it("returns the first index of the value in the array if the value exists", function() {
-//     expect(findIndex(animals, "duck")).toBe(0);
-//     expect(findIndex(animals, "cat")).toBe(1);
-//     expect(findIndex(animals, "pony")).toBe(3);
-//   });
+  it("returns the first index of the value in the array if the value exists", function() {
+    expect(findIndex(animals, "duck")).toBe(0);
+    expect(findIndex(animals, "cat")).toBe(1);
+    expect(findIndex(animals, "pony")).toBe(3);
+  });
 
-//   it("returns -1 if the value does not exist", function() {
-//     expect(findIndex(animals, "porcupine")).toBe(-1);
-//   });
-// });
+  it("returns -1 if the value does not exist", function() {
+    expect(findIndex(animals, "porcupine")).toBe(-1);
+  });
+});
 
-// describe("revString", function() {
-//   it("returns a reversed copy of the string", function() {
-//     expect(revString("")).toBe("");
-//     expect(revString("x")).toBe("x");
-//     expect(revString("cat")).toBe("tac");
-//     expect(revString("duck")).toBe("kcud");
-//   });
-// });
+describe("revString", function() {
+  it("returns a reversed copy of the string", function() {
+    expect(revString("")).toBe("");
+    expect(revString("x")).toBe("x");
+    expect(revString("cat")).toBe("tac");
+    expect(revString("duck")).toBe("kcud");
+  });
+});
 
-// describe("gatherStrings", function() {
-//   it("gathers all object string values into an array", function() {
-//     let whiskey = {
-//       name: "Whiskey",
-//       age: 5,
-//       favFood: "popcorn",
-//       color: "black",
-//       barks: false
-//     };
-//     expect(gatherStrings(whiskey).sort()).toEqual(["Whiskey", "popcorn", "black"].sort());
-//   });
+describe("gatherStrings", function() {
+  it("gathers all object string values into an array", function() {
+    let whiskey = {
+      name: "Whiskey",
+      age: 5,
+      favFood: "popcorn",
+      color: "black",
+      barks: false
+    };
+    expect(gatherStrings(whiskey).sort()).toEqual(["Whiskey", "popcorn", "black"].sort());
+  });
 
-//   it("handles nested objects", function() {
-//     let nestedObj = {
-//       firstName: "Lester",
-//       favoriteNumber: 22,
-//       moreData: {
-//         lastName: "Testowitz"
-//       },
-//       funFacts: {
-//         moreStuff: {
-//           anotherNumber: 100,
-//           deeplyNestedString: {
-//             almostThere: {
-//               success: "you made it!"
-//             }
-//           }
-//         },
-//         favoriteString: "nice!"
-//       }
-//     };
-//     expect(gatherStrings(nestedObj).sort()).toEqual(["Lester", "Testowitz", "you made it!", "nice!"].sort());
-//   });
-// });
+  it("handles nested objects", function() {
+    let nestedObj = {
+      firstName: "Lester",
+      favoriteNumber: 22,
+      moreData: {
+        lastName: "Testowitz"
+      },
+      funFacts: {
+        moreStuff: {
+          anotherNumber: 100,
+          deeplyNestedString: {
+            almostThere: {
+              success: "you made it!"
+            }
+          }
+        },
+        favoriteString: "nice!"
+      }
+    };
+    expect(gatherStrings(nestedObj).sort()).toEqual(["Lester", "Testowitz", "you made it!", "nice!"].sort());
+  });
+});
 
 // describe("binarySearch", function () {
 //   it("should find the index of a value in a sorted array", function() {
